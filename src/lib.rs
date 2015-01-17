93db058a4f779b611ffd345b5d1e86098171c7bb
Dominik Prien <dprien@users.noreply.github.com>
2015-01-17 20:12:54 +0100

    Add rustified http_parser.h declarations

---
+++ src/lib.rs
@@ -0,0 +1,97 @@
+#![allow(unstable)]
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(non_camel_case_types)]
+
+extern crate libc;
+
+type http_cb = extern fn (*mut http_parser) -> libc::c_int;
+type http_data_cb = extern fn (*mut http_parser, *const libc::c_char, libc::size_t) -> libc::c_int;
+
+enum http_parser_type
+{
+    HTTP_REQUEST,
+    HTTP_RESPONSE,
+    HTTP_BOTH
+}
+
+#[repr(C)]
+struct http_parser {
+    /** PRIVATE **/
+    _type: libc::c_uint,
+    _flags: libc::c_uint,
+    _state: libc::c_uint,
+    _header_state: libc::c_uint,
+    _index: libc::c_uint,
+
+    _nread: libc::uint32_t,
+    _content_length: libc::uint64_t,
+
+    _http_major: libc::c_ushort,
+    _http_minor: libc::c_ushort,
+    _status_code: libc::c_uint,
+    _method: libc::c_uint,
+    _http_errno: libc::c_uint,
+
+    _upgrade: libc::c_uint,
+
+    /** PUBLIC **/
+    data: *mut libc::c_void,
+}
+
+#[repr(C)]
+struct http_parser_settings {
+    on_message_begin: http_cb,
+    on_url: http_data_cb,
+    on_status: http_data_cb,
+    on_header_field: http_data_cb,
+    on_header_value: http_data_cb,
+    on_headers_complete: http_cb,
+    on_body: http_data_cb,
+    on_message_complete: http_cb,
+}
+
+extern "C" fn on_message_begin(parser: *mut http_parser) -> libc::c_int {
+    0
+}
+
+extern "C" fn on_url(parser: *mut http_parser, at: *const libc::c_char, length: libc::size_t) -> libc::c_int {
+    0
+}
+
+extern "C" fn on_status(parser: *mut http_parser, at: *const libc::c_char, length: libc::size_t) -> libc::c_int {
+    0
+}
+
+extern "C" fn on_header_field(parser: *mut http_parser, at: *const libc::c_char, length: libc::size_t) -> libc::c_int {
+    0
+}
+
+extern "C" fn on_header_value(parser: *mut http_parser, at: *const libc::c_char, length: libc::size_t) -> libc::c_int {
+    0
+}
+
+extern "C" fn on_headers_complete(parser: *mut http_parser) -> libc::c_int {
+    0
+}
+
+extern "C" fn on_body(parser: *mut http_parser, at: *const libc::c_char, length: libc::size_t) -> libc::c_int {
+    0
+}
+
+extern "C" fn on_message_complete(parser: *mut http_parser) -> libc::c_int {
+    0
+}
+
+#[link(name = "http-parser")]
+extern {
+    fn http_parser_init(
+        parser: *mut http_parser,
+        type_: libc::c_int) -> libc::c_void;
+
+    fn http_parser_execute(
+        parser: *mut http_parser,
+        settings: *const http_parser_settings,
+        data: *const libc::c_char,
+        len: libc::size_t) -> libc::size_t;
+}
